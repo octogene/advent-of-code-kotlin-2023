77a01166c96e1fbabcc1d4c3ab1abb5146d5715e
Bogdan Cordier <ooctogene@gmail.com>
2023-12-15 07:45:08 +0100

    :tada: Day15 Part II

--- src/Day15.kt
+++ src/Day15.kt
@@ -4,7 +4,26 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val regex = "^(\\w+)(=|-)(\\d+)?".toRegex()
+        val boxes = List<LinkedHashMap<String, Int>>(256) { LinkedHashMap() }
+        input.first().split(",").mapNotNull {
+            regex.find(it)?.groupValues?.drop(1)
+        }.forEach {
+            val (label, instruction, value) = it
+            if (instruction == "=") {
+                val boxNumber = hashAlgorithm(label)
+                boxes[boxNumber][label] = value.toInt()
+            } else {
+                val boxNumber = hashAlgorithm(label)
+                boxes[boxNumber].remove(label)
+            }
+        }
+        return boxes.mapIndexed { index, linkedHashMap ->
+            val boxFactor = index + 1
+            linkedHashMap.keys.mapIndexed { slot, s ->
+                boxFactor * (slot + 1) * linkedHashMap.getValue(s)
+            }.sum()
+        }.sum()
     }
 
     val input = readInput("Day15")
